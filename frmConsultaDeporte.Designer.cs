ccaf104c3d4803461622aebee1b004c2cfb870ad
Agustin Arla <agusa@DESKTOP-0BG6L2K>
2022-10-19 10:55:22 -0300

    Agregar archivos de proyecto.

---
+++ frmConsultaDeporte.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace pryArlaBD
+{
+    partial class frmConsultaDeporte
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grlDeportes = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grlDeportes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grlDeportes
+            // 
+            this.grlDeportes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grlDeportes.Location = new System.Drawing.Point(12, 11);
+            this.grlDeportes.Name = "grlDeportes";
+            this.grlDeportes.Size = new System.Drawing.Size(776, 300);
+            this.grlDeportes.TabIndex = 0;
+            // 
+            // frmConsultaDeporte
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.grlDeportes);
+            this.Name = "frmConsultaDeporte";
+            this.Text = "Consulta-Deporte";
+            ((System.ComponentModel.ISupportInitialize)(this.grlDeportes)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grlDeportes;
+    }
+}
---
+++ frmPrincipal.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace pryArlaBD
+{
+    public partial class frmPrincipal : Form
+    {
+        public OleDbConnection objConexion;
+        public OleDbCommand objDato;
+        public OleDbDataReader objLecutra;
+
+        public string varRuta = "DEPORTE.accdb";
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                lblFecha.Text = DateTime.Now.ToString();
+                objConexion = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + varRuta);
+                objConexion.Open();
+                lblEstado.Text = "Conectado: " + objConexion.ConnectionString;
+                stsConexion.BackColor = Color.GreenYellow;
+            }
+            catch (Exception Mensajito)
+            {
+                stsConexion.BackColor = Color.Red;
+                lblEstado.Text = Mensajito.Message;
+                //throw;
+            }
+        }
+
+        private void Conexion_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void consultaDeportistaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmConsultaDeporte objConsultaDeporte = new frmConsultaDeporte();
+            objConsultaDeporte.ShowDialog();
+            
+
+        }
+
+        private void consultaEntrenadorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmConsultaEntrenador objConsultaEntrenador = new frmConsultaEntrenador();
+            objConsultaEntrenador.ShowDialog();
+        }
+    }
+}
